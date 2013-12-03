25b0a3fb34aec5ed63e0897a3b4de9f903ea2ec4
xtr4nge <xtr4nge@gmail.com>
2013-12-03 13:15:07 +0000

    display module in status page

--- _info_.php
+++ _info_.php
@@ -4,6 +4,9 @@ $mod_version="1.0";
 $mod_logs="/usr/share/FruityWifi/logs/captive.log"; 
 $mod_logs_history="/usr/share/FruityWifi/www/modules/captive/includes/logs/";
 $mod_path="/usr/share/FruityWifi/www/modules/captive";
+$mod_panel="show";
+$mod_isup="/usr/share/FruityWifi/bin/danger \"/sbin/iptables -t mangle -L|grep -iEe 'internet.+anywhere'\"";
+$mod_alias="Captive";
 # EXEC
 $bin_danger = "/usr/share/FruityWifi/bin/danger";
 $bin_iptables = "/sbin/iptables";
